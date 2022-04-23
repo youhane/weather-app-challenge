30733e1703ca316566489a23691b495c67892fe9
youhane <jason.sebastian154@gmail.com>
2022-04-23 20:04:19 +0700

    [add] tried emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Heading } from './LeftDisplay/LeftDisplay.style';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Heading>Hello</Heading>
     </div>
   );
 }
---
+++ src/LeftDisplay/LeftDisplay.style.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Heading = styled('h1')`
+  background-color: #008f68;
+  color: #fae042;
+`;
