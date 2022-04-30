0c0d9a0e7a1818169cea9fc0f4859c0b987541b1
youhane <jason.sebastian154@gmail.com>
2022-04-30 23:59:10 +0700

    [add] added react-icons

--- src/Components/LeftDisplay/LeftDisplay.style.js
+++ src/Components/LeftDisplay/LeftDisplay.style.js
@@ -3,6 +3,15 @@ import styled from '@emotion/styled';
 export const Wrapper = styled('div')`
   background: #1E213A;
   height: 100vh;
+  width: 45vh;
+  padding: 2rem 1rem;
+
+  .search{
+    background: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `
 
 export const SearchButton = styled('button')`
--- src/Components/LeftDisplay/LeftDisplay.tsx
+++ src/Components/LeftDisplay/LeftDisplay.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { CircleButton, SearchButton, Wrapper } from './LeftDisplay.style'
-
+import { MdGpsFixed } from 'react-icons/md'
+import { IconContext } from 'react-icons'
 
 export default function LeftDisplay() {
   return (
     <Wrapper>
-        <SearchButton>Search for places</SearchButton>
-        <CircleButton>a</CircleButton>
+        <div className='search'>
+          <SearchButton>Search for places</SearchButton>
+          <CircleButton>
+            <IconContext.Provider value={{ style: {background: 'transparent'} }}>
+              <MdGpsFixed />
+            </IconContext.Provider>
+          </CircleButton>
+        </div>
     </Wrapper>
   )
 }
