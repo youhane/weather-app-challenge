3b727e9c718e7abe206762caa947721f7efa19ac
youhane <jason.sebastian154@gmail.com>
2022-07-18 16:33:49 +0700

    [add] location choice added

--- src/Components/SearchBar/SearchBar.style.js
+++ src/Components/SearchBar/SearchBar.style.js
@@ -3,13 +3,47 @@ import styled from "@emotion/styled";
 export const Wrapper = styled('div')`
     position: absolute;
     background: #1E213A;
+    width: 25vw;
+`
+
+export const CloseButton = styled('button')`
+    float: right;
+    margin-right: 1rem;
+    font-size: 1.3rem;
+
+    *{
+        color: #fff;
+    }
 `
 
 export const SearchInput = styled('div')`
-    background: #3C47E9;
     color: #E7E7EB;
+    display: flex;
+    margin: 1rem;
+    margin-top: 2rem;
+    justify-content: space-between;
+
+    input{
+        border: 1px solid #E7E7EB;
+        width: 15rem;
+        padding-left: .5rem;
+    }
+
+    button{
+        background: #3C47E9;
+        padding: .9rem;
+    }
 `
 
-export const Selection = styled('select')`
+export const Selection = styled('button')`
+    border: 1px solid #616475;
+    margin: 1rem;
+    margin-bottom: 0rem;
+    width: 21rem;
+    padding: .9rem;
+    text-align: left;
 
+    :hover{
+        background: #5D5F6A;
+    }
 `
--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,9 +1,11 @@
 import React, { useContext, useRef } from 'react'
-import { SearchInput, Selection, Wrapper } from './SearchBar.style'
+import { CloseButton, SearchInput, Selection, Wrapper } from './SearchBar.style'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { GrClose } from 'react-icons/gr'
-import { IconContext } from 'react-icons'
 import { WeatherContext } from '../Context/WeatherContext';
+import { IconContext } from 'react-icons';
+
+const CITIES = ['Jakarta', 'Singapore', 'Bangkok', 'New York', 'London', 'Kuala Lumpur', 'Tokyo', 'Seoul']
 
 export default function SearchBar() {
   const {location, setLocation} = useContext(WeatherContext)
@@ -11,16 +13,17 @@ export default function SearchBar() {
 
   return (
     <Wrapper>
-      <button><GrClose/></button>
+      <CloseButton>X</CloseButton>
+      <br />
       <SearchInput>
-        <input type='text' name='search' ref={locationRef} />
-        <button type='button' onClick={setLocation(locationRef)}>
-          <AiOutlineSearch size={20} /> Search
+        <input type='text' name='search' ref={locationRef} placeholder='Search Location' />
+        <button type='button' onClick={() => setLocation(locationRef.current.value)}>
+          Search
         </button>
       </SearchInput>
-      <Selection>
-
-      </Selection>
+      {CITIES.map((city, index) => (
+          <Selection key={index} value={city}>{city}</Selection>
+      ))}
     </Wrapper>
   )
 }
